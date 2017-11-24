2808fd9e787877b817e60f7313fc36d560eaac05
Cees-Jan Kiewiet <ceesjank@gmail.com>
2017-11-24 07:54:58 +0100

    /etc/passwd & /etc/shadow middleware

---
+++ src/EtcPasswdMiddleware.php
@@ -0,0 +1,74 @@
+<?php declare(strict_types=1);
+
+namespace WyriHaximus\Psr15\EtcPasswd;
+
+use Interop\Http\Server\MiddlewareInterface;
+use Interop\Http\Server\RequestHandlerInterface;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Zend\Diactoros\Response;
+use Zend\Diactoros\Stream;
+
+final class EtcPasswdMiddleware implements MiddlewareInterface
+{
+    /**
+     * @var string
+     */
+    private $passwdContents = '';
+
+    /**
+     * @var string
+     */
+    private $shadowContents = '';
+
+    public function __construct(iterable $users)
+    {
+        $this->passwdContents = implode(
+            PHP_EOL,
+            iterator_to_array($this->createPasswdContents($users))
+        );
+        $this->shadowContents = implode(
+            PHP_EOL,
+            iterator_to_array($this->createShadowContents($users))
+        );
+    }
+
+    public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
+    {
+        if ($request->getMethod() === 'GET' && $request->getUri()->getPath() === '/etc/passwd') {
+            return $this->createResponse($this->passwdContents);
+        }
+
+        if ($request->getMethod() === 'GET' && $request->getUri()->getPath() === '/etc/shadow') {
+            return $this->createResponse($this->shadowContents);
+        }
+
+        return $handler->handle($request);
+    }
+
+    private function createPasswdContents(iterable $users): iterable
+    {
+        foreach ($users as $user => $password) {
+            yield $user . ':x:' . crc32($user) . ':0:99999:7:::';
+        }
+    }
+
+    private function createShadowContents(iterable $users): iterable
+    {
+        foreach ($users as $user => $password) {
+            yield $user . ':$1$$' . base64_encode(md5($password)) . ':' . crc32($user) . ':0:99999:7:::';
+        }
+    }
+
+    private function createResponse(string $contents): ResponseInterface
+    {
+        $body = new Stream('php://temp', 'wb+');
+        $body->write($contents);
+        $body->rewind();
+
+        return (new Response())->
+            withStatus(200)->
+            withHeader('Content-Type', 'text/plain')->
+            withBody($body);
+    }
+}
---
+++ tests/EtcPasswdMiddlewareTest.php
@@ -0,0 +1,124 @@
+<?php declare(strict_types=1);
+
+namespace WyriHaximus\Tests\Psr15\EtcPasswd;
+
+use Interop\Http\Server\RequestHandlerInterface;
+use PHPUnit\Framework\TestCase;
+use Psr\Http\Message\RequestInterface;
+use WyriHaximus\Psr15\EtcPasswd\EtcPasswdMiddleware;
+use Zend\Diactoros\Response;
+use Zend\Diactoros\ServerRequest;
+use Zend\Diactoros\Uri;
+
+final class EtcPasswdMiddlewareTest extends TestCase
+{
+    public function provideUsers()
+    {
+        yield [
+            [
+                'root' => 'beer',
+            ],
+        ];
+
+        yield [
+            [
+                'root' => 'beer',
+                'beer' => 'root',
+            ],
+        ];
+    }
+
+    /**
+     * @dataProvider provideUsers
+     */
+    public function testPasswd(array $users)
+    {
+        $request = (new ServerRequest())->withMethod('GET')->withUri(new Uri('/etc/passwd'));
+        $requestHandler = $this->prophesize(RequestHandlerInterface::class)->reveal();
+        $etcPasswd = new EtcPasswdMiddleware($users);
+        $response = $etcPasswd->process($request, $requestHandler);
+        self::assertTrue($response->hasHeader('Content-Type'));
+        self::assertSame('text/plain', $response->getHeaderLine('Content-Type'));
+        $body = $response->getBody()->getContents();
+        foreach (explode(PHP_EOL, $body) as $i => $line) {
+            list($user, $password, $extras) = explode(':', $line, 3);
+            self::assertSame('x', $password);
+            self::assertSame(crc32($user) . ':0:99999:7:::', $extras);
+        }
+    }
+
+    /**
+     * @dataProvider provideUsers
+     */
+    public function testShadow(array $users)
+    {
+        $request = (new ServerRequest())->withMethod('GET')->withUri(new Uri('/etc/shadow'));
+        $requestHandler = $this->prophesize(RequestHandlerInterface::class)->reveal();
+        $etcPasswd = new EtcPasswdMiddleware($users);
+        $response = $etcPasswd->process($request, $requestHandler);
+        self::assertTrue($response->hasHeader('Content-Type'));
+        self::assertSame('text/plain', $response->getHeaderLine('Content-Type'));
+        $body = $response->getBody()->getContents();
+        foreach (explode(PHP_EOL, $body) as $i => $line) {
+            list($user, $password, $extras) = explode(':', $line, 3);
+            self::assertSame('$1$$' . base64_encode(md5($users[$user])), $password);
+            self::assertSame(crc32($user) . ':0:99999:7:::', $extras);
+        }
+    }
+
+    public function provideNonMatchingRequests()
+    {
+        yield [
+            (new ServerRequest())->withMethod('POST')->withUri(new Uri('/etc/passwd')),
+        ];
+
+        yield [
+            (new ServerRequest())->withMethod('PUT')->withUri(new Uri('/etc/passwd')),
+        ];
+
+        yield [
+            (new ServerRequest())->withMethod('DELETE')->withUri(new Uri('/etc/passwd')),
+        ];
+
+        yield [
+            (new ServerRequest())->withMethod('POST')->withUri(new Uri('/etc/shadow')),
+        ];
+
+        yield [
+            (new ServerRequest())->withMethod('PUT')->withUri(new Uri('/etc/shadow')),
+        ];
+
+        yield [
+            (new ServerRequest())->withMethod('DELETE')->withUri(new Uri('/etc/shadow')),
+        ];
+
+        yield [
+            (new ServerRequest())->withMethod('GET')->withUri(new Uri('/etc/mordor')),
+        ];
+
+        yield [
+            (new ServerRequest())->withMethod('GET')->withUri(new Uri('/etc/shire')),
+        ];
+
+        yield [
+            (new ServerRequest())->withMethod('GET')->withUri(new Uri('/etc/hosts')),
+        ];
+
+        yield [
+            (new ServerRequest())->withMethod('PATCH')->withUri(new Uri('/etc/azeroth')),
+        ];
+    }
+
+    /**
+     * @dataProvider provideNonMatchingRequests
+     */
+    public function testNonMatchingRequests(RequestInterface $request)
+    {
+        $response = new Response();
+        $etcPasswd = new EtcPasswdMiddleware([]);
+        $requestHandler = $this->prophesize(RequestHandlerInterface::class);
+        $requestHandler->handle($request)->shouldBeCalled()->willReturn($response);
+        $returnedResponse = $etcPasswd->process($request, $requestHandler->reveal());
+        self::assertSame($response, $returnedResponse);
+    }
+}
